db25645b08c14a54c80c07d9322a186989666a29
doraKoreny <dorakoreny@gmail.com>
2018-07-09 10:37:13 +0200

    user guess checking problem is solved, output problem is solved

--- play_guess_number.py
+++ play_guess_number.py
@@ -5,26 +5,26 @@ def give_exercise(name, lowest, highest):
     print("Well %s, I am thinking of a number between %d and %d." % (name, lowest, highest))
 
 
-def check_guess(guess, number, name, guessing_round):
+def check_guess(guess, number):
     if guess != number:
         if guess > number:
-            print(input("Your guess is too high.\nTake a guess.\n"))
+            print("Your guess is too high.")
         elif guess < number:
-            print(input("Your guess is too low.\nTake a guess.\n"))
-    elif guess == number:
-        print("Good job, %s! You guessed my number in %d guesses!" % (name, guessing_round))
-        
+            print("Your guess is too low.")
+
 
 def main():
-    random_number = guess_number(1,20)
+    random_number = guess_number(1, 20)
     print(random_number)
     player_name = input("Hello! What's your name?")
     give_exercise(player_name, 1, 20)
     count_guesses = 0
     user_guess = int(input("Take a guess\n"))
     while user_guess != random_number:
-        check_guess(user_guess, random_number, player_name, count_guesses)
+        check_guess(user_guess, random_number)
         count_guesses += 1
+        user_guess = int(input("Take a guess\n"))
+    print("Good job, %s! You guessed my number in %d guesses!" % (player_name, count_guesses))
 
 
 if __name__ == '__main__':
