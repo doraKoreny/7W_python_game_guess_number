228c63876dbbc5ea4d775c4b611de761efd2e1c2
doraKoreny <dorakoreny@gmail.com>
2018-07-09 10:39:11 +0200

    generate random number

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,13 @@
 import random
 
+
 def guess_number(lowest, highest):
-    return random.randint(lowest,highest)
+    return random.randint(lowest, highest)
+
 
 def main():
-    guess_number(1,20)
-    
+    guess_number(1, 20)
 
-    
+
 if __name__ == '__main__':
     main()
