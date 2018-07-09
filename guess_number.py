4b169ae855621b86b286ff60e3000fe2eb8df310
doraKoreny <dorakoreny@gmail.com>
2018-07-09 10:02:32 +0200

    printing not perfect

---
+++ guess_number.py
@@ -0,0 +1,12 @@
+import random
+
+def guess_number(lowest, highest):
+    return random.randint(lowest,highest)
+
+def main():
+    guess_number(1,20)
+    
+
+    
+if __name__ == '__main__':
+    main()
